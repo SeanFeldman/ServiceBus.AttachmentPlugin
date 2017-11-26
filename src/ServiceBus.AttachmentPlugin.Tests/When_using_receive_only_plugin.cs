e5d928161f829b86333927f6a3ee79547c155ffa
Sean Feldman <feldman.sean@gmail.com>
2017-11-26 02:07:02 -0700

    Add ability to use attachment plugin in read-only mode using SAS URI

---
+++ src/ServiceBus.AttachmentPlugin.Tests/When_using_receive_only_plugin.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ServiceBus.AttachmentPlugin.Tests
+{
+    using System;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Xunit;
+
+    public class When_using_receive_only_plugin : IClassFixture<AzureStorageEmulatorFixture>
+    {
+        [Fact]
+        public async Task Should_download_attachment_using_provided_from_sas_uri()
+        {
+            var payload = "payload";
+            var bytes = Encoding.UTF8.GetBytes(payload);
+            var message = new Message(bytes)
+            {
+                MessageId = Guid.NewGuid().ToString(),
+            };
+            var plugin = new AzureStorageAttachment(new AzureStorageAttachmentConfiguration(
+                    connectionString: "UseDevelopmentStorage=true", containerName: "attachments", messagePropertyToIdentifyAttachmentBlob: "attachment-id")
+                    .WithSasUri(sasTokenValidationTime: TimeSpan.FromHours(4), messagePropertyToIdentifySasUri: "mySasUriProperty"));
+            await plugin.BeforeMessageSend(message);
+
+
+            var receiveOnlyPlugin = new ReceiveOnlyAzureStorageAttachment("mySasUriProperty");
+            var result = await receiveOnlyPlugin.AfterMessageReceive(message);
+
+            Assert.True(message.UserProperties.ContainsKey("mySasUriProperty"));
+            Assert.Equal(payload, Encoding.UTF8.GetString(result.Body));
+        }
+    }
+}
---
+++ src/ServiceBus.AttachmentPlugin/ReceiveOnlyAzureStorageAttachment.cs
@@ -0,0 +1,45 @@
+﻿namespace ServiceBus.AttachmentPlugin
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Azure.ServiceBus.Core;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+
+    class ReceiveOnlyAzureStorageAttachment : ServiceBusPlugin
+    {
+        string messagePropertyToIdentifySasUri;
+
+        public ReceiveOnlyAzureStorageAttachment(string messagePropertyToIdentifySasUri)
+        {
+            this.messagePropertyToIdentifySasUri = messagePropertyToIdentifySasUri;
+        }
+
+        public override string Name { get; } = nameof(ReceiveOnlyAzureStorageAttachment);
+
+        public override async Task<Message> AfterMessageReceive(Message message)
+        {
+            var userProperties = message.UserProperties;
+            if (!userProperties.ContainsKey(messagePropertyToIdentifySasUri))
+            {
+                return message;
+            }
+
+            var blob = new CloudBlockBlob(new Uri(userProperties[messagePropertyToIdentifySasUri].ToString()));
+            try
+            {
+                await blob.FetchAttributesAsync().ConfigureAwait(false);
+            }
+            catch (StorageException exception)
+            {
+                throw new Exception($"Blob with name '{blob.Name}' under container '{blob.Container.Name}' cannot be found.", exception);
+            }
+            var fileByteLength = blob.Properties.Length;
+            var bytes = new byte[fileByteLength];
+            await blob.DownloadToByteArrayAsync(bytes, 0).ConfigureAwait(false);
+            message.Body = bytes;
+            return message;
+        }
+    }
+}
