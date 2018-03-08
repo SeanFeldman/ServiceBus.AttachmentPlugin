7086cd1351fe63b304da621b5c6488f62422952e
Sean Feldman <feldman.sean@gmail.com>
2018-03-08 15:41:19 -0700

    Add plain text connection string provider

---
+++ src/ServiceBus.AttachmentPlugin/PlainTextConnectionStringProvider.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceBus.AttachmentPlugin
+{
+    /// <summary>
+    /// Static connection string provider.
+    /// </summary>
+    public class PlainTextConnectionStringProvider : IProvideStorageConnectionString
+    {
+        readonly string connectionString;
+
+        /// <summary></summary>
+        /// <param name="connectionString">Storage connection string to use in plain text.</param>
+        public PlainTextConnectionStringProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// Return connection string for Storage account.
+        /// </summary>
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}
