b0025bbbe6bf5d098922edfff1c10e3c9150ef5f
Sean Feldman <feldman.sean@gmail.com>
2018-03-08 15:39:56 -0700

    Add abstraction for connection string

---
+++ src/ServiceBus.AttachmentPlugin/IProvideStorageConnectionString.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceBus.AttachmentPlugin
+{
+    /// <summary>
+    /// Storage account connection string provider.
+    /// </summary>
+    public interface IProvideStorageConnectionString
+    {
+        /// <summary>
+        /// Connection string for storage account to be used.
+        /// </summary>
+        string GetConnectionString();
+    }
+}
