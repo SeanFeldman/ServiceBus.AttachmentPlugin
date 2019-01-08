dc1a81f3740f0776f817936ad211611274d1c977
Sean Feldman <feldman.sean@gmail.com>
2019-01-07 22:51:13 -0700

    Plugin name should be reported the same consistently
    Fixes #76

--- src/ServiceBus.AttachmentPlugin/ReceiveOnlyAzureStorageAttachment.cs
+++ src/ServiceBus.AttachmentPlugin/ReceiveOnlyAzureStorageAttachment.cs
@@ -16,7 +16,7 @@
             this.messagePropertyToIdentifySasUri = messagePropertyToIdentifySasUri;
         }
 
-        public override string Name { get; } = nameof(ReceiveOnlyAzureStorageAttachment);
+        public override string Name { get; } = nameof(AzureStorageAttachment);
 
         public override async Task<Message> AfterMessageReceive(Message message)
         {
