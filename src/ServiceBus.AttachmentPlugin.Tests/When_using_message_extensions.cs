a8380fc3b1b324d4dfec2dbf91ca50daede4e09a
Ludvig Falck <lfalck@gmail.com>
2019-03-13 09:52:06 +0100

    Added extension methods for working with attachments without registering plugin.

---
+++ src/ServiceBus.AttachmentPlugin.Tests/When_using_message_extensions.cs
@@ -0,0 +1,35 @@
+﻿namespace ServiceBus.AttachmentPlugin.Tests
+{
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Xunit;
+
+    public class When_using_message_extensions : IClassFixture<AzureStorageEmulatorFixture>
+    {
+        readonly AzureStorageEmulatorFixture fixture;
+
+        public When_using_message_extensions(AzureStorageEmulatorFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public async Task Should_send_and_receive()
+        {
+            var payload = "payload";
+            var bytes = Encoding.UTF8.GetBytes(payload);
+            var message = new Message(bytes);
+            var configuration = new AzureStorageAttachmentConfiguration(
+                connectionStringProvider: AzureStorageEmulatorFixture.ConnectionStringProvider, containerName: "attachments", messagePropertyToIdentifyAttachmentBlob: "attachment-id");
+
+            await message.UploadAzureStorageAttachment(configuration);
+
+            Assert.Null(message.Body);
+
+            var receivedMessage = await message.DownloadAzureStorageAttachment(configuration);
+
+            Assert.Equal(payload, Encoding.UTF8.GetString(receivedMessage.Body));
+        }
+    }
+}
---
+++ src/ServiceBus.AttachmentPlugin/MessageExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Microsoft.Azure.ServiceBus
+{
+    using global::ServiceBus.AttachmentPlugin;
+    using System.Threading.Tasks;
+
+    /// <summary>Extension methods for working with attachments without registering plugin.</summary>
+    public static class MessageExtensions
+    {
+        /// <summary>Upload attachment to Azure Storage blob without registering plugin.</summary>
+        /// <param name="message"><see cref="Message"/></param>
+        /// <param name="configuration"><see cref="AzureStorageAttachmentConfiguration"/> object.</param>
+        /// <returns><see cref="Message"/> with body uploaded to Azure Storage blob.</returns>
+        public static async Task<Message> UploadAzureStorageAttachment(this Message message, AzureStorageAttachmentConfiguration configuration)
+        {
+            var plugin = new AzureStorageAttachment(configuration);
+            return await plugin.BeforeMessageSend(message).ConfigureAwait(false);
+        }
+
+        /// <summary>Download attachment from Azure Storage blob without registering plugin.</summary>
+        /// <param name="message"><see cref="Message"/></param>
+        /// <param name="configuration"><see cref="AzureStorageAttachmentConfiguration"/> object.</param>
+        /// <returns><see cref="Message"/> with body downloaded from Azure Storage blob.</returns>
+        public static async Task<Message> DownloadAzureStorageAttachment(this Message message, AzureStorageAttachmentConfiguration configuration)
+        {
+            var plugin = new AzureStorageAttachment(configuration);
+            return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
+        }
+    }
+}
