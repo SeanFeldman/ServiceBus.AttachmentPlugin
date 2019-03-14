9d4821aac7837548e19effbcff7334ff3aeedd11
Ludvig Falck <lfalck@gmail.com>
2019-03-14 22:36:10 +0100

    Added two extension methods which use ReceiveOnlyAzureStorageAttachment.

--- src/ServiceBus.AttachmentPlugin.Tests/When_using_message_extensions.cs
+++ src/ServiceBus.AttachmentPlugin.Tests/When_using_message_extensions.cs
@@ -15,7 +15,7 @@
         }
 
         [Fact]
-        public async Task Should_send_and_receive()
+        public async Task Should_send_and_receive_with_configuration()
         {
             var payload = "payload";
             var bytes = Encoding.UTF8.GetBytes(payload);
@@ -28,6 +28,45 @@
             Assert.Null(message.Body);
 
             var receivedMessage = await message.DownloadAzureStorageAttachment(configuration);
+
+            Assert.Equal(payload, Encoding.UTF8.GetString(receivedMessage.Body));
+        }
+
+        [Fact]
+        public async Task Should_send_and_receive_with_default_sas_uri_property()
+        {
+            var payload = "payload";
+            var bytes = Encoding.UTF8.GetBytes(payload);
+            var message = new Message(bytes);
+            var configuration = new AzureStorageAttachmentConfiguration(
+                connectionStringProvider: AzureStorageEmulatorFixture.ConnectionStringProvider, containerName: "attachments", messagePropertyToIdentifyAttachmentBlob: "attachment-id")
+            .WithBlobSasUri();
+
+            await message.UploadAzureStorageAttachment(configuration);
+
+            Assert.Null(message.Body);
+
+            var receivedMessage = await message.DownloadAzureStorageAttachment();
+
+            Assert.Equal(payload, Encoding.UTF8.GetString(receivedMessage.Body));
+        }
+
+        [Fact]
+        public async Task Should_send_and_receive_with_custom_sas_uri_property()
+        {
+            var payload = "payload";
+            var bytes = Encoding.UTF8.GetBytes(payload);
+            var message = new Message(bytes);
+            string customSasUri = "$custom-attachment.sas.uri";
+            var configuration = new AzureStorageAttachmentConfiguration(
+                connectionStringProvider: AzureStorageEmulatorFixture.ConnectionStringProvider, containerName: "attachments", messagePropertyToIdentifyAttachmentBlob: "attachment-id")
+            .WithBlobSasUri(customSasUri);
+
+            await message.UploadAzureStorageAttachment(configuration);
+
+            Assert.Null(message.Body);
+
+            var receivedMessage = await message.DownloadAzureStorageAttachment(customSasUri);
 
             Assert.Equal(payload, Encoding.UTF8.GetString(receivedMessage.Body));
         }
--- src/ServiceBus.AttachmentPlugin/MessageExtensions.cs
+++ src/ServiceBus.AttachmentPlugin/MessageExtensions.cs
@@ -16,13 +16,33 @@
             return await plugin.BeforeMessageSend(message).ConfigureAwait(false);
         }
 
-        /// <summary>Download attachment from Azure Storage blob without registering plugin.</summary>
+        /// <summary>Download attachment from Azure Storage blob without registering plugin, using configuration object.</summary>
         /// <param name="message"><see cref="Message"/></param>
         /// <param name="configuration"><see cref="AzureStorageAttachmentConfiguration"/> object.</param>
         /// <returns><see cref="Message"/> with body downloaded from Azure Storage blob.</returns>
         public static async Task<Message> DownloadAzureStorageAttachment(this Message message, AzureStorageAttachmentConfiguration configuration)
         {
             var plugin = new AzureStorageAttachment(configuration);
+            return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
+        }
+
+        /// <summary>Download attachment from Azure Storage blob without registering plugin, using default message property to identify SAS URI.</summary>
+        /// <param name="message"><see cref="Message"/></param>
+        /// <returns><see cref="Message"/> with body downloaded from Azure Storage blob.</returns>
+        public static async Task<Message> DownloadAzureStorageAttachment(this Message message)
+        {
+            var plugin = new ReceiveOnlyAzureStorageAttachment(AzureStorageAttachmentConfigurationExtensions.DefaultMessagePropertyToIdentitySasUri);
+            return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
+        }
+
+
+        /// <summary>Download attachment from Azure Storage blob without registering plugin, with custom message property to identify SAS URI.</summary>
+        /// <param name="message"><see cref="Message"/></param>
+        /// <param name="messagePropertyToIdentifySasUri">Message property which contains the SAS URI used to fetch message body from blob.</param>
+        /// <returns><see cref="Message"/> with body downloaded from Azure Storage blob.</returns>
+        public static async Task<Message> DownloadAzureStorageAttachment(this Message message, string messagePropertyToIdentifySasUri)
+        {
+            var plugin = new ReceiveOnlyAzureStorageAttachment(messagePropertyToIdentifySasUri);
             return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
         }
     }
