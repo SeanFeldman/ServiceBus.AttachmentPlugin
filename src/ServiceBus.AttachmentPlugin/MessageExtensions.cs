2e28fdb2be5c6ef09dfc406f4ff6ede3868e7a7c
Ludvig Falck <lfalck@gmail.com>
2019-03-15 09:27:21 +0100

    Removed redundant DownloadAzureStorageAttachment extension method.

--- src/ServiceBus.AttachmentPlugin/MessageExtensions.cs
+++ src/ServiceBus.AttachmentPlugin/MessageExtensions.cs
@@ -26,23 +26,13 @@
             return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
         }
 
-        /// <summary>Download attachment from Azure Storage blob without registering plugin, using default message property to identify SAS URI.</summary>
-        /// <param name="message"><see cref="Message"/></param>
-        /// <returns><see cref="Message"/> with body downloaded from Azure Storage blob.</returns>
-        public static async Task<Message> DownloadAzureStorageAttachment(this Message message)
-        {
-            var plugin = new ReceiveOnlyAzureStorageAttachment(AzureStorageAttachmentConfigurationExtensions.DefaultMessagePropertyToIdentitySasUri);
-            return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
-        }
-
-        /// <summary>Download attachment from Azure Storage blob without registering plugin, with custom message property to identify SAS URI.</summary>
+        /// <summary>Download attachment from Azure Storage blob without registering plugin, using message property to identify SAS URI.</summary>
         /// <param name="message"><see cref="Message"/></param>
         /// <param name="messagePropertyToIdentifySasUri">Message property which contains the SAS URI used to fetch message body from blob.</param>
         /// <returns><see cref="Message"/> with body downloaded from Azure Storage blob.</returns>
-        public static async Task<Message> DownloadAzureStorageAttachment(this Message message, string messagePropertyToIdentifySasUri)
+        public static async Task<Message> DownloadAzureStorageAttachment(this Message message, string messagePropertyToIdentifySasUri = AzureStorageAttachmentConfigurationExtensions.DefaultMessagePropertyToIdentitySasUri)
         {
             var plugin = new ReceiveOnlyAzureStorageAttachment(messagePropertyToIdentifySasUri);
             return await plugin.AfterMessageReceive(message).ConfigureAwait(false);
         }
     }
-}
