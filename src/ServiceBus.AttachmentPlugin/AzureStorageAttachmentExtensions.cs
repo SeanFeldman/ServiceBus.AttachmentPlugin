a355d0b364bb1cd7e43fbd6f5ee6d2e8708a90de
Saurabh Rajguru <saurabh.nospam@gmail.com>
2019-06-21 22:03:52 +0530

    Use client entity contract instead of implementation. (#114)
    * Use client entity contract instead of implementation.
    
    * Revert "Use client entity contract instead of implementation."
    
    This reverts commit 1914dc8ccf07f274bce5300238a51a5381f545f9.
    
    * Added overloads for Registration with IClientEntity.
    
    * Updated API approvals.

--- src/ServiceBus.AttachmentPlugin/AzureStorageAttachmentExtensions.cs
+++ src/ServiceBus.AttachmentPlugin/AzureStorageAttachmentExtensions.cs
@@ -31,5 +31,31 @@
 
             return plugin;
         }
+
+        /// <summary>Instantiate plugin with the required configuration.</summary>
+        /// <param name="client"><see cref="QueueClient"/>, <see cref="SubscriptionClient"/>, <see cref="QueueClient"/>, <see cref="MessageSender"/>, <see cref="MessageReceiver"/>, or <see cref="SessionClient"/> to register plugin with.</param>
+        /// <param name="configuration"><see cref="AzureStorageAttachmentConfiguration"/> object.</param>
+        /// <returns>Registered plugin as <see cref="ServiceBusPlugin"/>.</returns>
+        public static ServiceBusPlugin RegisterAzureStorageAttachmentPlugin(this IClientEntity client, AzureStorageAttachmentConfiguration configuration)
+        {
+            var plugin = new AzureStorageAttachment(configuration);
+
+            client.RegisterPlugin(plugin);
+
+            return plugin;
+        }
+
+        /// <summary>Initiate plugin for Receive-Only mode to retrieve attachments using SAS URI. </summary>
+        /// <param name="client"><see cref="QueueClient"/>, <see cref="SubscriptionClient"/>, <see cref="QueueClient"/>, <see cref="MessageSender"/>, <see cref="MessageReceiver"/>, or <see cref="SessionClient"/> to register plugin with.</param>
+        /// <param name="messagePropertyToIdentifySasUri">Message property name to be used to retrieve message SAS UI.</param>
+        /// <returns>Registered plugin as <see cref="ServiceBusPlugin"/>.</returns>
+        public static ServiceBusPlugin RegisterAzureStorageAttachmentPluginForReceivingOnly(this IClientEntity client, string messagePropertyToIdentifySasUri = AzureStorageAttachmentConfigurationExtensions.DefaultMessagePropertyToIdentitySasUri)
+        {
+            var plugin = new ReceiveOnlyAzureStorageAttachment(messagePropertyToIdentifySasUri);
+
+            client.RegisterPlugin(plugin);
+
+            return plugin;
+        }
     }
 }
