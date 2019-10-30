22c960a6454f92fb6db409d3e06b4afb2471842a
Sean Feldman <feldman.sean@gmail.com>
2019-10-29 22:17:05 -0600

    Remove overloads taking a specific client that are not needed. Fixes #115

--- src/ServiceBus.AttachmentPlugin/AzureStorageAttachmentExtensions.cs
+++ src/ServiceBus.AttachmentPlugin/AzureStorageAttachmentExtensions.cs
@@ -7,24 +7,6 @@
     public static class AzureStorageAttachmentExtensions
     {
         /// <summary>Instantiate plugin with the required configuration.</summary>
-        /// <param name="client"><see cref="ClientEntity"/>, <see cref="SubscriptionClient"/>, <see cref="QueueClient"/>, <see cref="MessageSender"/>, <see cref="MessageReceiver"/>, or <see cref="SessionClient"/> to register plugin with.</param>
-        /// <param name="configuration"><see cref="AzureStorageAttachmentConfiguration"/> object.</param>
-        /// <returns>Registered plugin as <see cref="ServiceBusPlugin"/>.</returns>
-        public static ServiceBusPlugin RegisterAzureStorageAttachmentPlugin(this ClientEntity client, AzureStorageAttachmentConfiguration configuration)
-        {
-            return RegisterAzureStorageAttachmentPlugin((IClientEntity)client, configuration);
-        }
-
-        /// <summary>Initiate plugin for Receive-Only mode to retrieve attachments using SAS URI. </summary>
-        /// <param name="client"><see cref="ClientEntity"/>, <see cref="SubscriptionClient"/>, <see cref="QueueClient"/>, <see cref="MessageSender"/>, <see cref="MessageReceiver"/>, or <see cref="SessionClient"/> to register plugin with.</param>
-        /// <param name="messagePropertyToIdentifySasUri">Message property name to be used to retrieve message SAS UI.</param>
-        /// <returns>Registered plugin as <see cref="ServiceBusPlugin"/>.</returns>
-        public static ServiceBusPlugin RegisterAzureStorageAttachmentPluginForReceivingOnly(this ClientEntity client, string messagePropertyToIdentifySasUri = AzureStorageAttachmentConfigurationExtensions.DefaultMessagePropertyToIdentitySasUri)
-        {
-            return RegisterAzureStorageAttachmentPluginForReceivingOnly((IClientEntity)client, messagePropertyToIdentifySasUri);
-        }
-
-        /// <summary>Instantiate plugin with the required configuration.</summary>
         /// <param name="client"><see cref="IClientEntity"/>, <see cref="SubscriptionClient"/>, <see cref="QueueClient"/>, <see cref="MessageSender"/>, <see cref="MessageReceiver"/>, or <see cref="SessionClient"/> to register plugin with.</param>
         /// <param name="configuration"><see cref="AzureStorageAttachmentConfiguration"/> object.</param>
         /// <returns>Registered plugin as <see cref="ServiceBusPlugin"/>.</returns>
