c21ff33774093954075a78223061aa741f79c44e
Sean Feldman <feldman.sean@gmail.com>
2020-06-02 23:54:11 -0600

    failing tests to verify build is working
    Signed-off-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/ServiceBus.AttachmentPlugin.Tests/DummyTests.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceBus.AttachmentPlugin.Tests
+{
+    using Xunit;
+
+    public class DummyTests
+    {
+
+        [Fact]
+        public static void ShouldFail()
+        {
+            Assert.False(true);
+        }
+    }
+}
